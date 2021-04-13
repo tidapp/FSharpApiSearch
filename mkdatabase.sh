9c26049ca2a637075c6be233df8d538830fab621
Robert Petranovic <robi.petranovic@gmail.com>
2021-04-13 12:49:23 +0200

    linux support

--- mkdatabase.sh
+++ mkdatabase.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-mkdir -p lib/
-#find /mnt/c/Users/rpetrano/projects/cloud/fn/build/homepage/ -iname '*.dll' -exec cp '{}' lib/ \;
+: ${projectdir:=~/projects/tidapp/cloud/fn/build/homepage}
+: ${nugetdir:=~/.nuget/packages}
 
-./bin/FSharpApiSearch.Database/net5.0/FSharpApiSearch.Database.exe "--FSharpCore:C:/Program Files/dotnet/sdk/5.0.201/FSharp" "--Framework:C:/Program Files/dotnet/shared/Microsoft.AspNetCore.App/5.0.4/" "--Framework:C:/Program Files/dotnet/shared/Microsoft.NETCore.App/5.0.4/" --lib:lib/ mscorlib FSharp.Core $(for i in lib/*.dll; do basename $i; done)
+libs=$(
+    find "$projectdir" -iname '*.fsproj' -exec grep -iF '<PackageReference' {} \; |
+    sed -e 's:.*Include="\([^"]\+\)".*:\1.dll:i' |
+    sort -u
+)
+
+mkdir -p lib/
+find "$nugetdir" | grep -iFf <( echo "$libs" ) | sort -n | xargs -I{} -r cp {} lib/
+
+if uname -a | grep -qi microsoft; then
+    exec ./bin/FSharpApiSearch.Database/net5.0/FSharpApiSearch.Database.exe "--FSharpCore:C:/Program Files/dotnet/sdk/5.0.201/FSharp" "--Framework:C:/Program Files/dotnet/shared/Microsoft.AspNetCore.App/5.0.4/" "--Framework:C:/Program Files/dotnet/shared/Microsoft.NETCore.App/5.0.4/" --lib:lib/ mscorlib FSharp.Core $(for i in lib/*.dll; do basename $i; done)
+else
+    exec ./bin/FSharpApiSearch.Database/net5.0/FSharpApiSearch.Database "--FSharpCore:/usr/share/dotnet/sdk/5.0.201/FSharp/" "--Framework:/usr/share/dotnet/shared/Microsoft.NETCore.App/5.0.4/" "--Framework:/usr/share/dotnet/shared/Microsoft.AspNetCore.App/5.0.4/" --lib:lib/ mscorlib FSharp.Core $(for i in lib/*.dll; do basename $i; done)
+fi
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./bin/FSharpApiSearch.Console/net5.0/FSharpApiSearch.Console.exe "$@" -t:mscorlib -t:FSharp.Core $(for i in lib/*.dll; do echo -n "-t:$(basename $i | sed -e s/.dll$//i) "; done)
+if uname -a | grep -qi microsoft; then
+    ext=.exe
+fi
+
+exec ./bin/FSharpApiSearch.Console/net5.0/FSharpApiSearch.Console$ext "$@" -t:mscorlib -t:FSharp.Core $(for i in lib/*.dll; do echo -n "-t:$(basename $i | sed -e s/.dll$//i) "; done)
